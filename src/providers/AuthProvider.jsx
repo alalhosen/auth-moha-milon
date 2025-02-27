c406e5d20c18e0d19f89a50b21eba2f60aefb233
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-27 20:56:36 +0600

    recap AuthProvider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,24 +1,24 @@
-import PropTypes from "prop-types";
 import { createContext } from "react";
+import PropTypes from 'prop-types'; 
 
-export const AuthContext = createContext(null);
+const AuthContext = createContext(null);
 
-const AuthProvider = ({ children }) => {
-  const authInfo = { name: "Nodi Sagoooooor" };
-  return;
-  <AuthContext.Provider value={authInfo}>
-    {children}
-  </AuthContext.Provider>;
+const AuthProvider = () => {
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default AuthProvider;
 
 AuthProvider.propTypes = {
-  children: PropTypes.node,
-};
+  children: PropTypes.node
+}
 
 /***
- * 1. Create context and expirt it
+ * 1. Create context and export it
  * 2. set provider with value
  * 3. use the Auth Provider in the main.jsx file
  * 4. access children in the AuthProvider component as children use it in the middle of the Provider
