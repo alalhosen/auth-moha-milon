fed9b4197e01c0effd05dbef4075ed9227ff4840
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-27 23:43:23 +0600

    createUserWithEmailAndPassword add

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -8,6 +8,10 @@ const AuthProvider = ({children}) => {
 const [user, setUser] = useState(null);
 
 
+const createUser = (email, password) => {
+  return createUserWithEmailAndPassword (auth, email, password)
+}
+
   const authInfo = {user}
   return (
     <AuthContext.Provider value={authInfo}>
